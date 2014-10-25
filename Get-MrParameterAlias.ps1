d1f91efe36d84dbfec672d8c7a6bf604b6355099
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2014-10-25 17:35:04 -0500

    Added Get-MrParameterAlias PowerShell Function

---
+++ Get-MrParameterAlias.ps1
@@ -0,0 +1,12 @@
+﻿function Get-MrParameterAlias {
+
+    [CmdletBinding()]
+    param (
+        [string]$Name
+    )
+
+    (Get-Command -Name $Name).Parameters.Values |
+    Where-Object Aliases |
+    Select-Object -Property Name, Aliases
+
+}
