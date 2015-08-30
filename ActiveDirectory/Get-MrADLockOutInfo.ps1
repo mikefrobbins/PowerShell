5e6c96905f663eac3bc7f7ab7c5fbbe2c87609ed
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2015-08-30 13:09:20 -0500

    Documentation Updates & Code Changes to Get-MrADLockOutInfo
    Removed begin and process blocks since there is not pipeline input.
    Change credential for Invoke-Command to splatting since it's an optional
    parameter. Updated the comment based help to reflect these changes.

--- ActiveDirectory/Get-MrADLockOutInfo.ps1
+++ ActiveDirectory/Get-MrADLockOutInfo.ps1
@@ -7,20 +7,23 @@ function Get-MrADLockOutInfo {
   
 .DESCRIPTION 
     Get-MrADLockOutInfo is an advanced function that returns a list of users who were locked out in Active Directory 
-    by querying the event logs on the PDC emulation in the domain. 
+    by querying the event logs on the PDC emulator in the domain. 
   
 .PARAMETER UserName 
     The userid of the specific user you are looking for lockouts for. The default is all locked out users. 
   
 .PARAMETER StartTime 
-    The datetime to start searching from. The default is all datetimes that exist in the event logs.
+    The datetime to start searching the event logs from. The default is the past three days.
 
 .PARAMETER Credential
-    Specifies a user account that has permission to read the security event log on the PDC emulator in the forest root
-    domain. The default is the current user.
+    Specifies a user account that has permission to read the security event log on the PDC emulator. The default is
+    the current user.
   
 .EXAMPLE 
-    Get-MrADLockOutInfo 
+    Get-MrADLockOutInfo
+
+.EXAMPLE
+    Get-MrADLockOutInfo -Credential (Get-Credential)
   
 .EXAMPLE 
     Get-MrADLockOutInfo -UserName 'mikefrobbins' 
@@ -29,7 +32,7 @@ function Get-MrADLockOutInfo {
     Get-MrADLockOutInfo -StartTime (Get-Date).AddDays(-1) 
   
 .EXAMPLE 
-    Get-MrADLockOutInfo -UserName 'mikefrobbins' -StartTime (Get-Date).AddDays(-1) 
+    Get-MrADLockOutInfo -UserName 'mikefrobbins' -StartTime (Get-Date).AddDays(-1) -Credential (Get-Credential)
 #> 
  
     [CmdletBinding()] 
@@ -46,31 +49,31 @@ function Get-MrADLockOutInfo {
         [System.Management.Automation.Credential()]$Credential = [System.Management.Automation.PSCredential]::Empty
     )
 
-    BEGIN {
-        try {
-            $ErrorActionPreference = 'Stop'
+    try {
+        $ErrorActionPreference = 'Stop'
 
-            $PdcEmulator = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain(( 
-                New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext('Domain', $DomainName)) 
-            ).PdcRoleOwner.name
+        $PdcEmulator = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain(( 
+            New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext('Domain', $DomainName)) 
+        ).PdcRoleOwner.name
 
-            Write-Verbose -Message "The PDC emulator in your forest root domain is: $PdcEmulator"
-            $ErrorActionPreference = 'Continue'
-        }
-        catch {
-            Write-Error -Message 'Unable to query the domain. Verify the user running this script has read access to Active Directory and try again.'
-        }    
+        Write-Verbose -Message "The PDC emulator in your forest root domain is: $PdcEmulator"
+        $ErrorActionPreference = 'Continue'
+    }
+    catch {
+        Write-Error -Message 'Unable to query the domain. Verify the user running this script has read access to Active Directory and try again.'
     }
     
-    PROCESS {
-        Invoke-Command -ComputerName $PdcEmulator { 
- 
-            Get-WinEvent -FilterHashtable @{LogName='Security';Id=4740;StartTime=$Using:StartTime} |
-            Where-Object {$_.Properties[0].Value -like "$Using:UserName"} |
-            Select-Object -Property TimeCreated,
-                                    @{Label='UserName';Expression={$_.Properties[0].Value}},
-                                    @{Label='ClientName';Expression={$_.Properties[1].Value}}
-        } -Credential $Credential | 
-        Select-Object -Property TimeCreated, UserName, ClientName
+    $Params = @{}
+    If ($PSBoundParameters['Credential']) {
+        $Params.Credential = $Credential
     }
+
+    Invoke-Command -ComputerName $PdcEmulator { 
+        Get-WinEvent -FilterHashtable @{LogName='Security';Id=4740;StartTime=$Using:StartTime} |
+        Where-Object {$_.Properties[0].Value -like "$Using:UserName"} |
+        Select-Object -Property TimeCreated,
+                                @{Label='UserName';Expression={$_.Properties[0].Value}},
+                                @{Label='ClientName';Expression={$_.Properties[1].Value}}
+    } @Params | 
+    Select-Object -Property TimeCreated, UserName, ClientName
 }
