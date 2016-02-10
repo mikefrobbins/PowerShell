39083a892c133610259fbef813177fa2dd04d263
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-02-10 16:05:42 -0600

    Added functions to get and remove credentials from Windows Credential Manager

---
+++ Get-MrSavedCredential.ps1
@@ -0,0 +1,31 @@
+﻿#Requires -Version 3.0
+function Get-MrSavedCredential {
+
+    [CmdletBinding()]
+    param (
+        [ValidateScript({
+            If ($_ -notmatch '^\/') {
+                $True
+            }
+            else {
+                Throw "$_ is not valid. Target cannot begin with a forward slash."
+            }
+        })]
+        [string]$Target
+    )
+
+    $data = & "$env:windir\System32\cmdkey.exe" /list $Target |
+            Select-String -Pattern '^\s+\S'
+
+    for ($i=0;$i -lt $data.count;$i+=4) {
+
+        [pscustomobject] @{
+            Target = $data[$i] -replace '^.*=|^.*:\s|\s*$'
+            Type = $data[$i+1] -replace '^.*:\s|\s*$'
+            User = $data[$i+2] -replace '^.*:\s|\s*$'
+            Persistence = $data[$i+3] -replace '^\s*|\s\S*$'
+        }
+
+    }
+
+}
---
+++ Remove-MrSavedCredential.ps1
@@ -0,0 +1,18 @@
+﻿#Requires -Version 3.0
+function Remove-MrSavedCredential {
+
+    [CmdletBinding(SupportsShouldProcess,
+                   ConfirmImpact='Medium')]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipelineByPropertyName)]
+        [string]$Target
+    )
+
+    PROCESS {
+        if ($PSCmdlet.ShouldProcess($Target,'Delete')) {
+            & "$env:windir\System32\cmdkey.exe" /delete $Target
+        }
+    }
+    
+}
