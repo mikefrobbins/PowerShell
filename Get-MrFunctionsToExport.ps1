121b0c88b4255ce49a5558618ba59fb03a8c8066
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-02-15 15:11:25 -0600

    Added Get-MrFunctionsToExport function via PS1 file

---
+++ Get-MrFunctionsToExport.ps1
@@ -0,0 +1,35 @@
+﻿function Get-MrFunctionsToExport {
+
+    [CmdletBinding()]
+    param (
+        [ValidateScript({
+          If (Test-Path -Path $_ -PathType Container) {
+            $True
+          }
+          else {
+            Throw "'$_' is not a valid directory."
+          }
+        })]
+        [string]$Path = (Get-Location),
+
+        [string]$Exclude = 'profile.ps1',
+
+        [switch]$Recurse
+    )
+
+    $Params = @{
+        Exclude = $Exclude
+    }
+
+    if ($PSBoundParameters.Recurse) {
+        $Params.Recurse = $true
+    }
+
+    $results = (Get-ChildItem -Path "$Path\*.ps1" @Params |
+    Select-Object -ExpandProperty BaseName) -join "', '"
+        
+    if ($results) {
+        Write-Output "'$results'"
+    }
+    
+}
