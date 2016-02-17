9f7da229936f75b561095469cd31477684f3d651
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-02-16 18:09:07 -0600

    Added Get-MrExceptionType function via PS1 file

---
+++ Get-MrExceptionType.ps1
@@ -0,0 +1,20 @@
+﻿function Get-MrExceptionType {
+
+    [CmdletBinding()]
+    param (
+        [ValidateRange(1,256)]
+        [int]$Count = 1
+    )
+    
+    for ($i = 0; $i -lt $Count; $i++) {
+
+        if ($Error[$i]) {
+            [PSCustomObject]@{
+                ErrorNumber = "`$Error[$i]"
+                ExceptionType = if ($Error[$i].exception) {$Error[$i].Exception.GetType().FullName}
+            }
+        }
+
+    }
+    
+}
