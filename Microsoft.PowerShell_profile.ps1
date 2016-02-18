90e3568262d7eec0ffa34c3dde8b1ce657223aa4
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-02-18 10:09:51 -0600

    Added all users PowerShell console profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+$Now = Get-Date
+$Date = Get-Date -Month $Now.Month -Day 1
+$Book = (Invoke-WebRequest -Uri https://www.packtpub.com/packt/offers/free-learning/).ParsedHtml.getElementsByTagName('H2')[0].InnerHTML.Trim()
+
+while ($Date.DayOfWeek -ne 'Tuesday') {$Date = $Date.AddDays(1)}
+if ($Date.ToShortDateString() -eq $Now.ToShortDateString()) {
+    Update-Module -Force
+    Update-Help -ErrorAction SilentlyContinue
+}
+
+Write-Host 'The Packt Publishing free learning eBook of the day is: ' -ForegroundColor Cyan -NoNewline
+Write-Host "'$Book'" -ForegroundColor Yellow
