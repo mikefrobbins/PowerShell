32ca61eb2f8c3658d811d61702e5b0b32a42cc92
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-02-28 19:30:52 -0600

    Added logic to prevent error when error variable count is zero

--- Get-MrExceptionType.ps1
+++ Get-MrExceptionType.ps1
@@ -6,17 +6,24 @@
         [int]$Count = 1
     )
     
-    if ($Count -gt $Error.Count) {
-        $Count = $Error.Count
-    }
+    if ($Error.Count -ge 1) {
 
-    for ($i = 0; $i -lt $Count; $i++) {
-
-        [PSCustomObject]@{
-            ErrorNumber = "`$Error[$i]"
-            ExceptionType = if ($Error[$i].exception) {$Error[$i].Exception.GetType().FullName}
+        if ($Count -gt $Error.Count) {
+            $Count = $Error.Count
         }
 
+        for ($i = 0; $i -lt $Count; $i++) {
+
+            [PSCustomObject]@{
+                ErrorNumber = "`$Error[$i]"
+                ExceptionType = if ($Error[$i].exception) {$Error[$i].Exception.GetType().FullName}
+            }
+
+        }
+
+    }
+    else {
+        Write-Warning -Message 'No errors have been generated for the current PowerShell session.'
     }
     
 }
