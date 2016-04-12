1749e304c5fed11ef8cb15ad3376ec37ace3b4a7
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-04-12 11:03:51 -0500

    Check for Internet connectivity while suppressing warnings and progress

--- MrToolkit/Microsoft.PowerShell_profile.ps1
+++ MrToolkit/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,20 @@
-$Now = Get-Date
-$Date = Get-Date -Month $Now.Month -Day 1
-$Book = (Invoke-WebRequest -Uri https://www.packtpub.com/packt/offers/free-learning/).ParsedHtml.getElementsByTagName('H2')[0].InnerHTML.Trim()
+$ProgressPreference = 'SilentlyContinue'
 
-while ($Date.DayOfWeek -ne 'Tuesday') {$Date = $Date.AddDays(1)}
-if ($Date.ToShortDateString() -eq $Now.ToShortDateString()) {
-    Update-Module -Force
-    Update-Help -ErrorAction SilentlyContinue
+if (Test-NetConnection -ComputerName bing.com -Port 80 -InformationLevel Quiet -ErrorAction SilentlyContinue 3>$null) {
+
+    $Now = Get-Date
+    $Date = Get-Date -Month $Now.Month -Day 1
+    $Book = (Invoke-WebRequest -Uri https://www.packtpub.com/packt/offers/free-learning/).ParsedHtml.getElementsByTagName('H2')[0].InnerHTML.Trim()
+
+    while ($Date.DayOfWeek -ne 'Tuesday') {$Date = $Date.AddDays(1)}
+    if ($Date.ToShortDateString() -eq $Now.ToShortDateString()) {
+        Update-Module -Force
+        Update-Help -ErrorAction SilentlyContinue
+    }
+
+    Write-Host 'The Packt Publishing free learning eBook of the day is: ' -ForegroundColor Cyan -NoNewline
+    Write-Host "'$Book'" -ForegroundColor Yellow
+
 }
 
-Write-Host 'The Packt Publishing free learning eBook of the day is: ' -ForegroundColor Cyan -NoNewline
-Write-Host "'$Book'" -ForegroundColor Yellow
+$ProgressPreference = 'Continue'
