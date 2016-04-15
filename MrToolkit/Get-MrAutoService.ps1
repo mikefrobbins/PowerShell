ae4eaf4befad1c7b3a37f19b3b25a62c93dc40cf
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-04-15 14:01:25 -0500

    Updated Get-MrAutoService to be compatible with PowerShell v2 and higher

--- MrToolkit/Get-MrAutoService.ps1
+++ MrToolkit/Get-MrAutoService.ps1
@@ -64,9 +64,9 @@
         Invoke-Command @Params {
             $Services = Get-WmiObject -Class Win32_Service -Filter {
                 StartMode = 'Auto'
-            }
+            } -Property Name | Select-Object -ExpandProperty Name
             
-            foreach ($Service in $Services.Name) {
+            foreach ($Service in $Services) {
                 Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\$Service" |
                 Where-Object {$_.Start -eq 2 -and $_.DelayedAutoStart -ne 1} |
                 Select-Object -Property @{label='ServiceName';expression={$_.PSChildName}} |
