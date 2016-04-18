a46c38d5587cfdcfa041318711d4f76d3a81bb3f
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-04-18 10:58:24 -0500

    Added Get-MrUptime function and updated FunctionsToExport in manifest

---
+++ MrToolkit/Get-MrUptime.ps1
@@ -0,0 +1,19 @@
+﻿function Get-MrUptime {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline)]
+        [Microsoft.Management.Infrastructure.CimSession[]]$CimSession
+    )
+
+    PROCESS {
+        [array]$CimSessions += $CimSession
+    }
+    
+    END {
+        Get-CimInstance -CimSession $CimSessions -ClassName Win32_OperatingSystem -Property LocalDateTime, LastBootUpTime |
+        Select-Object -Property PSComputerName, @{label='Uptime';expression={$_.LocalDateTime - $_.LastBootUpTime}}
+
+        $CimSessions | Remove-CimSession -ErrorAction SilentlyContinue
+    }
+
+}
