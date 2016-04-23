1613be9ef818bc6282a6baa6c98f7a3ac272e23b
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-04-23 15:12:15 -0500

    Added folder for private functions and Test-MrFileLock function

---
+++ MrToolkit/Private/Test-MrFileLock.ps1
@@ -0,0 +1,28 @@
+﻿function Test-MrFileLock {
+    [CmdletBinding()]
+    param (
+        [ValidateScript({
+          If (Test-Path -Path $_ -PathType Leaf) {
+            $True
+          }
+          else {
+            Throw "'$($_ -replace '^.*\\')' is not a valid file."
+          }
+        })]
+        [string]$Path
+    )
+
+    try {
+        $File = [System.IO.File]::Open("$Path", 'Open', 'Read', 'ReadWrite')
+        if ($File) {
+            $File.Close()
+            $false            
+        }
+
+    }
+    catch {
+        Write-Verbose -Message "The file '$($Path -replace '^.*\\')' is locked by a process."
+        $true
+    }    
+
+}
