58e1c9a926728837cc72bddf10a2156ad4d8b188
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-04-28 15:15:41 -0500

    Added Get-MrPipelineInput function and updated FunctionsToExport in manifest

---
+++ MrToolkit/Get-MrPipelineInput.ps1
@@ -0,0 +1,25 @@
+﻿#Requires -Version 3.0
+function Get-MrPipelineInput {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$Name,
+        
+        [System.Management.Automation.WhereOperatorSelectionMode]$Option = 'Default',
+        
+        [ValidateRange(1,2147483647)]
+        [int]$Records = 2147483647
+    )
+
+    (Get-Command -Name $Name).ParameterSets.Parameters.Where({
+        $_.ValueFromPipeline -or $_.ValueFromPipelineByPropertyName
+    }, $Option, $Records).ForEach({
+        [pscustomobject]@{
+            ParameterName = $_.Name
+            ParameterType = $_.ParameterType
+            ValueFromPipeline = $_.ValueFromPipeline
+            ValueFromPipelineByPropertyName = $_.ValueFromPipelineByPropertyName
+        }
+    })
+
+}
