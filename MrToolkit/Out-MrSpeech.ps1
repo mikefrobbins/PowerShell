702bf6eb53c977abc017a3437eadb63fa67deae0
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-05-19 13:54:54 -0500

    Added Out-MrSpeech function

---
+++ MrToolkit/Out-MrSpeech.ps1
@@ -0,0 +1,15 @@
+﻿function Out-MrSpeech {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipeline)]
+        [string]$Phrase
+    )
+
+    Add-Type -AssemblyName System.Speech
+
+    $voice = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+
+    $voice.Speak($Phrase)
+
+}
