80dc63fa1cfbcd4e3caca0eb127648667f8fefd3
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-05-19 13:54:14 -0500

    New-MrScriptModule function

---
+++ MrToolkit/Invoke-MrPesterToSpeech.ps1
@@ -0,0 +1,19 @@
+﻿function Invoke-MrPesterToSpeech {
+    [CmdletBinding()]
+    param (
+        [switch]$Quiet
+    )
+
+    $Params = @{}
+    if ($PSBoundParameters.Quiet) {
+        $Params.Quiet = $true
+    }
+
+    $Results = Invoke-Pester -PassThru @Params |
+    Select-Object -ExpandProperty TestResult
+
+    foreach ($Result in $Results) {
+        Write-Output "The unit test named $($Result.Name) has $($Result.Result)." |
+        Out-MrSpeech
+    }
+}
---
+++ MrToolkit/New-MrScriptModule.ps1
@@ -0,0 +1,38 @@
+﻿function New-MrScriptModule {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$Name,
+        
+        [ValidateScript({
+          If (Test-Path -Path $_ -PathType Container) {
+            $true
+          }
+          else {
+            Throw "'$_' is not a valid directory."
+          }
+        })]
+        [String]$Path,
+
+        [Parameter(Mandatory)]
+        [string]$Author,
+
+        [Parameter(Mandatory)]
+        [string]$CompanyName,
+
+        [Parameter(Mandatory)]
+        [string]$Description,
+
+        [Parameter(Mandatory)]
+        [string]$PowerShellVersion
+    )
+
+    Out-File -FilePath (Join-Path -Path $Path -ChildPath "$Name.psm1") -Encoding utf8
+    Add-Content -Path (Join-Path -Path $Path -ChildPath "$Name.psm1") -Value '#Dot source all functions in all ps1 files located in the module folder
+Get-ChildItem -Path $PSScriptRoot\*.ps1 -Exclude *.tests.ps1, *profile.ps1 |
+ForEach-Object {
+    . $_.FullName
+}'
+    New-ModuleManifest -Path (Join-Path -Path $Path -ChildPath "$Name.psd1") -RootModule $Name -Author $Author -Description $Description -CompanyName $CompanyName `
+    -PowerShellVersion $PowerShellVersion -AliasesToExport $null -FunctionsToExport $null -VariablesToExport $null -CmdletsToExport $null
+}
