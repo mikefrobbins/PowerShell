18c78529562de7eb6656c37f471afcfbc24bbec8
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-06-09 15:41:05 -0500

    Added function to generate template for a new function

---
+++ MrToolkit/New-MrFunction.ps1
@@ -0,0 +1,69 @@
+﻿function New-MrFunction {
+    [CmdletBinding()]
+    param (
+        [string]$Name,
+        [string]$Path
+    )
+
+    New-Fixture -Path $Path -Name $Name
+    Set-Content -Path (Join-Path -Path $Path -ChildPath "$Name.ps1") -Force -Value "function $($Name) {
+
+<#
+.SYNOPSIS
+    Brief synopsis about the function.
+ 
+.DESCRIPTION
+    Detailed explanation of the purpose of this function.
+ 
+.PARAMETER Param1
+    The purpose of param1.
+
+.PARAMETER Param2
+    The purpose of param2.
+ 
+.EXAMPLE
+     $($Name) -Param1 'Value1', 'Value2'
+
+.EXAMPLE
+     'Value1', 'Value2' | $($Name)
+
+.EXAMPLE
+     $($Name) -Param1 'Value1', 'Value2' -Param2 'Value'
+ 
+.INPUTS
+    String
+ 
+.OUTPUTS
+    PSCustomObject
+ 
+.NOTES
+    Author:  Mike F Robbins
+    Website: http://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, 
+                   ValueFromPipeline)]
+        [string[]]`$Param1,
+
+        [ValidateNotNullOrEmpty()]
+        [string]`$Param2
+    )
+
+    BEGIN {
+
+    }
+
+    PROCESS {
+
+    }
+
+    END {
+
+    }
+
+}"
+
+}
