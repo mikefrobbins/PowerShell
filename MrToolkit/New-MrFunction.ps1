7210dc26064e5a314efc9a11a901c87c6ea43fad
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-07-13 14:35:04 -0500

    Added comment based help and approved verb validation to New-MrFunction

--- MrToolkit/New-MrFunction.ps1
+++ MrToolkit/New-MrFunction.ps1
@@ -1,13 +1,66 @@
-﻿#Requires -Modules Pester
+﻿#Requires -Version 3.0 -Modules Pester
 function New-MrFunction {
+
+<#
+.SYNOPSIS
+    Creates a new PowerShell function in the specified location.
+ 
+.DESCRIPTION
+    New-MrFunction is an advanced function that creates a new PowerShell function in the
+    specified location including creating a Pester test for the new function.
+ 
+.PARAMETER Name
+    Name of the function.
+
+.PARAMETER Path
+    Path of the location where to create the function. This location must already exist.
+ 
+.EXAMPLE
+     New-MrFunction -Name Get-MrPSVersion -Path "$env:ProgramFiles\WindowsPowerShell\Modules\MyModule"
+
+.INPUTS
+    None
+ 
+.OUTPUTS
+    System.IO.FileInfo
+ 
+.NOTES
+    Author:  Mike F Robbins
+    Website: http://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
     [CmdletBinding()]
+    [OutputType('System.IO.FileInfo')]
     param (
+        [ValidateScript({
+          If ((Get-Verb -Verb ($_ -replace '-.*$')).Verb) {
+            $true
+          }
+          else {
+            Throw "'$_' does NOT use an approved Verb."
+          }
+        })]
         [string]$Name,
+
+        [ValidateScript({
+          If (Test-Path -Path $_ -PathType Container) {
+            $true
+          }
+          else {
+            Throw "'$_' is not a valid directory."
+          }
+        })]
         [string]$Path
     )
 
-    New-Fixture -Path $Path -Name $Name
-    Set-Content -Path (Join-Path -Path $Path -ChildPath "$Name.ps1") -Force -Value "function $($Name) {
+    $FunctionPath = Join-Path -Path $Path -ChildPath "$Name.ps1"
+
+    if (-not(Test-Path -Path $FunctionPath)) {
+    
+        New-Fixture -Path $Path -Name $Name
+        Set-Content -Path $FunctionPath -Force -Value "#Requires -Version 3.0
+function $($Name) {
 
 <#
 .SYNOPSIS
@@ -44,6 +97,7 @@ function New-MrFunction {
 #>
 
     [CmdletBinding()]
+    [OutputType('PSCustomObject')]
     param (
         [Parameter(Mandatory, 
                    ValueFromPipeline)]
@@ -54,17 +108,26 @@ function New-MrFunction {
     )
 
     BEGIN {
-
+        #Used for prep. This code runs one time prior to processing items specified via pipeline input.
     }
 
     PROCESS {
+        #This code runs one time for each item specified via pipeline input.
 
+        foreach (`$Param in `$Param1) {
+            #Use foreach scripting construct to make parameter input work the same as pipeline input (iterate through the specified items one at a time).
+        }
     }
 
     END {
-
+        #Used for cleanup. This code runs one time after all of the items specified via pipeline input are processed.
     }
 
 }"
+    
+    }
+    else {
+        Write-Error -Message 'Unable to create function. Specified file already exists!'
+    }    
 
 }
