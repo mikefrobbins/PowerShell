0e0d84fd96d2a853c324bef4280fe776c5cff719
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-09-14 16:59:22 -0500

    Added ConvertTo-MrHashTable function and updated FunctionsToExport in manifest

---
+++ MrToolkit/ConvertTo-MrHashtable.ps1
@@ -0,0 +1,19 @@
+﻿function ConvertTo-MrHashTable {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipeline)]
+        [PSObject[]]$Object
+    )
+    PROCESS {
+        foreach ($o in $Object) {
+            $hashtable = @{}
+            
+            foreach ($p in Get-Member -InputObject $o -MemberType Property) {
+                $hashtable.($p.Name) = $o.($p.Name)
+            }
+
+            Write-Output $hashtable
+        }
+    }
+}
