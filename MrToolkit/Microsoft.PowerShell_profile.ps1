6c10928d9c019f9e71625e3f0032f22c8bd557da
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-09-16 13:54:17 -0500

    Added logic for running as non-admin and IE first-launch config not being complete

--- MrToolkit/Microsoft.PowerShell_profile.ps1
+++ MrToolkit/Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,54 @@
-$ProgressPreference = 'SilentlyContinue'
+$Global:ProgressPreference = 'SilentlyContinue'
 
-if (Test-NetConnection -ComputerName bing.com -Port 80 -InformationLevel Quiet -ErrorAction SilentlyContinue 3>$null) {
+if (Test-NetConnection -ComputerName bing.com -Port 80 -InformationLevel Quiet -ErrorAction SilentlyContinue -WarningAction SilentlyContinue) {
 
     $Now = Get-Date
     $Date = Get-Date -Month $Now.Month -Day 1
-    $Book = (Invoke-WebRequest -Uri https://www.packtpub.com/packt/offers/free-learning/).ParsedHtml.getElementsByTagName('H2')[0].InnerHTML.Trim()
-
+    
     while ($Date.DayOfWeek -ne 'Tuesday') {$Date = $Date.AddDays(1)}
+        
     if ($Date.ToShortDateString() -eq $Now.ToShortDateString()) {
-        Update-Module
-        Update-Help -ErrorAction SilentlyContinue
+
+        $PSLUPath = "$env:ProgramFiles\WindowsPowerShell\Configuration\pshelp-lastupdated.txt"
+
+        $PSHelpLastUpdate = (Get-ChildItem -Path $PSLUPath -ErrorAction SilentlyContinue).LastWriteTime 
+
+        if ($PSHelpLastUpdate.Month -ne $Now.Month) {
+
+            if ((New-Object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+
+                New-Item -Path $PSLUPath -ItemType File -Force
+                
+                Start-Job {
+                    Update-Module -Force
+                    Update-Help -ErrorAction SilentlyContinue
+                } | Out-Null
+
+            }
+            else {
+                Write-Warning -Message 'Aborting PowerShell Module and Help update due to PowerShell not being run as a local administrator!'
+            }
+
+        }
+
+    }   
+
+    
+    try {
+        $Book = Invoke-WebRequest -Uri https://www.packtpub.com/packt/offers/free-learning/ -ErrorAction Stop
+    }
+    catch [System.NotSupportedException] {
+        Write-Warning -Message "Internet Explorer engine not available or its first-launch configuration is not complete."
+    }
+    catch {
+        Write-Warning -Message 'An unknown error has occurred.'
     }
 
-    Write-Host 'The Packt Publishing free learning eBook of the day is: ' -ForegroundColor Cyan -NoNewline
-    Write-Host "'$Book'" -ForegroundColor Yellow
+    if ($Book) {
+        Write-Host 'The Packt Publishing free learning eBook of the day is: ' -ForegroundColor Cyan -NoNewline
+        Write-Host "'$($Book.ParsedHtml.getElementsByTagName('H2')[0].InnerHTML.Trim())'" -ForegroundColor Yellow    
+    }
 
 }
 
-$ProgressPreference = 'Continue'
+$Global:ProgressPreference = 'Continue'
