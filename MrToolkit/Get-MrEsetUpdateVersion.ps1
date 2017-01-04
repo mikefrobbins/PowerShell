16c0be13db8cb2d9ed2f4241fd0c2d604840ea95
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2017-01-04 10:15:56 -0600

    Added Get-MrEsetUpdateVersion function

---
+++ MrToolkit/Get-MrEsetUpdateVersion.ps1
@@ -0,0 +1,28 @@
+﻿function Get-MrEsetUpdateVersion {
+    [CmdletBinding()]
+    param (
+        [ValidateNotNullOrEmpty()]
+        [string[]]$ComputerName = $env:COMPUTERNAME,
+        
+        [System.Management.Automation.Credential()]$Credential = [System.Management.Automation.PSCredential]::Empty
+    )
+
+    $Params = @{}
+    if ($PSBoundParameters.Credential){
+        $Params.Credential = $Credential
+    }
+
+    $Results = Invoke-Command -ComputerName $ComputerName {
+        Get-ItemProperty -Path 'HKLM:\SOFTWARE\ESET\ESET Security\CurrentVersion\Info' 2>&1 
+    } @Params
+
+    foreach ($Result in $Results) {
+        [pscustomobject]@{
+            ComputerName = $Result.PSComputerName
+            ProductName = $Result.ProductName
+            ScannerVersion = $Result.ScannerVersionId
+            LastUpdate = if ($Result.ScannerVersion) {([datetime]::ParseExact($Result.ScannerVersion -replace '^.*\(|\)', 'yyyyMMdd', $null)).ToShortDateString()}
+        }
+    }
+    
+}
