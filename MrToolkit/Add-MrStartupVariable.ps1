7cac3074cbbef6ba70e69629ad22a297029d20f8
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2017-07-19 14:03:13 -0500

    Added Add-MrStartupVariable and Remove-MrUserVariable functions

---
+++ MrToolkit/Add-MrStartupVariable.ps1
@@ -0,0 +1,55 @@
+﻿#Requires -Version 3.0
+function Add-MrStartupVariable {
+
+<#
+.SYNOPSIS
+    Add variable for list of startup variables.
+ 
+.DESCRIPTION
+    Add variable for list of startup variables to the specified PowerShell profile. Create the specified PowerShell
+    profile if it does not exist. Only adds the variable and code to populate the variable if it does not already exist.
+    Designed to be used in conjunction with Remove-MrUserVariable.
+ 
+.PARAMETER Location
+    Location of the PowerShell profile to add the startup variable to.
+ 
+.EXAMPLE
+     Add-MrStartupVariable -Location AllUsersCurrentHost
+
+.INPUTS
+    None
+ 
+.OUTPUTS
+    None
+ 
+.NOTES
+    Author:  Mike F Robbins
+    Website: http://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateSet('AllUsersAllHosts', 'AllUsersCurrentHost', 'CurrentUserAllHosts', 'CurrentUserCurrentHost')]
+        $Location
+    )
+
+    $Content = @'
+$StartupVars = @()
+$StartupVars = Get-Variable | Select-Object -ExpandProperty Name
+'@
+
+    if (-not(Test-Path -Path $profile.$Location)) {
+        New-Item -Path $profile.$Location -ItemType File |
+        Set-Content -Value $Content
+    }
+    elseif (-not(Get-Content -Path $profile.$Location |
+             Select-String -SimpleMatch '$StartupVars = Get-Variable | Select-Object -ExpandProperty Name')) {
+        Add-Content -Path $profile.$Location -Value "`r`n$Content"
+    }
+    else {
+        Write-Verbose -Message "`$StartupVars already exists in '$($profile.$Location)'"
+    }
+
+}
---
+++ MrToolkit/Remove-MrUserVariable.ps1
@@ -0,0 +1,50 @@
+﻿#Requires -Version 3.0
+function Remove-MrUserVariable {
+
+<#
+.SYNOPSIS
+    Removes user defined variables.
+ 
+.DESCRIPTION
+    Removes user defined variables from the PowerShell ISE or console. $StartupVars must be defined prior to running
+    this function, preferably in a profile script. Populate $StartUpVars with 'Get-Variable | Select-Object -ExpandProperty
+    Name'. All variables added after populating $StartupVars will be removed when this function is run.
+ 
+.EXAMPLE
+     Remove-MrUserVariable
+
+.INPUTS
+    None
+ 
+.OUTPUTS
+    None
+ 
+.NOTES
+    Author:  Mike F Robbins
+    Website: http://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param ()
+
+    if ($StartupVars) {
+        $UserVars = Get-Variable -Exclude $StartupVars -Scope Global
+        
+        foreach ($var in $UserVars){
+            try {
+                Remove-Variable -Name $var.Name -Force -Scope Global -ErrorAction Stop
+                Write-Verbose -Message "Variable '$($var.Name)' has been successfully removed."
+            }
+            catch {
+                Write-Warning -Message "An error has occured. Error Details: $($_.Exception.Message)"
+            }            
+            
+        }
+        
+    }
+    else {
+        Write-Warning -Message '$StartupVars has not been added to your PowerShell profile'
+    }    
+
+}
