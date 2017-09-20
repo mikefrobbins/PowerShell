2d12ec57e7dfb24c936a38b07d0c376759d47329
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2017-09-20 17:35:36 -0500

    Added Get/Set Internet Connection Sharing functions

---
+++ MrToolkit/Get-MrInternetConnectionSharing.ps1
@@ -0,0 +1,77 @@
+﻿#Requires -Version 3.0
+function Get-MrInternetConnectionSharing {
+
+<#
+.SYNOPSIS
+    Retrieves the status of Internet connection sharing for the specified network adapter(s).
+ 
+.DESCRIPTION
+    Get-MrInternetConnectionSharing is an advanced function that retrieves the status of Internet connection sharing
+    for the specified network adapter(s).
+ 
+.PARAMETER InternetInterfaceName
+    The name of the network adapter(s) to check the Internet connection sharing status for.
+ 
+.EXAMPLE
+    Get-MrInternetConnectionSharing -InternetInterfaceName Ethernet, 'Internal Virtual Switch'
+
+.EXAMPLE
+    'Ethernet', 'Internal Virtual Switch' | Get-MrInternetConnectionSharing
+
+.EXAMPLE
+    Get-NetAdapter | Get-MrInternetConnectionSharing
+
+.INPUTS
+    String
+ 
+.OUTPUTS
+    PSCustomObject
+ 
+.NOTES
+    Author:  Mike F Robbins
+    Website: http://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipeline,
+                   ValueFromPipelineByPropertyName)]
+        [Alias('Name')]
+        [string[]]$InternetInterfaceName
+    )
+
+    BEGIN {
+        regsvr32.exe /s hnetcfg.dll
+        $netShare = New-Object -ComObject HNetCfg.HNetShare
+    }
+
+    PROCESS {
+        foreach ($Interface in $InternetInterfaceName){
+        
+            $publicConnection = $netShare.EnumEveryConnection |
+            Where-Object {
+                $netShare.NetConnectionProps.Invoke($_).Name -eq $Interface
+            }
+            
+            try {
+                $Results = $netShare.INetSharingConfigurationForINetConnection.Invoke($publicConnection)
+            }
+            catch {
+                Write-Warning -Message "An unexpected error has occurred for network adapter: '$Interface'"
+                Return
+            }
+
+            [pscustomobject]@{
+                Name = $Interface
+                SharingEnabled = $Results.SharingEnabled
+                SharingConnectionType = $Results.SharingConnectionType
+                InternetFirewallEnabled = $Results.InternetFirewallEnabled
+            }
+            
+        }
+    
+    }    
+
+}
---
+++ MrToolkit/Set-MrInternetConnectionSharing.ps1
@@ -0,0 +1,117 @@
+﻿#Requires -Version 3.0 -Modules NetAdapter
+function Set-MrInternetConnectionSharing {
+
+<#
+.SYNOPSIS
+    Configures Internet connection sharing for the specified network adapter(s).
+ 
+.DESCRIPTION
+    Set-MrInternetConnectionSharing is an advanced function that configures Internet connection sharing
+    for the specified network adapter(s). The specified network adapter(s) must exist and must be enabled.
+    To enable Internet connection sharing, Internet connection sharing cannot already be enabled on any
+    network adapters.
+ 
+.PARAMETER InternetInterfaceName
+    The name of the network adapter to enable or disable Internet connection sharing for.
+ 
+ .PARAMETER LocalInterfaceName
+    The name of the network adapter to share the Internet connection with.
+
+ .PARAMETER Enabled
+    Boolean value to specify whether to enable or disable Internet connection sharing.
+
+.EXAMPLE
+    Set-MrInternetConnectionSharing -InternetInterfaceName Ethernet -LocalInterfaceName 'Internal Virtual Switch' -Enabled $true
+
+.EXAMPLE
+    'Ethernet' | Set-MrInternetConnectionSharing -LocalInterfaceName 'Internal Virtual Switch' -Enabled $false
+
+.EXAMPLE
+    Get-NetAdapter -Name Ethernet | Set-MrInternetConnectionSharing -LocalInterfaceName 'Internal Virtual Switch' -Enabled $true
+
+.INPUTS
+    String
+ 
+.OUTPUTS
+    PSCustomObject
+ 
+.NOTES
+    Author:  Mike F Robbins
+    Website: http://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+                   ValueFromPipeline,
+                   ValueFromPipelineByPropertyName)]
+        [ValidateScript({
+                    If ((Get-NetAdapter -Name $_).status -ne 'Disabled') {
+                        $True
+                    }
+                    else {
+                        Throw "$_ is either not a valid network adapter of it's currently disabled."
+                    }
+        })]
+        [Alias('Name')]
+        [string]$InternetInterfaceName,
+
+        [ValidateScript({
+                    If ((Get-NetAdapter -Name $_).status -ne 'Disabled') {
+                        $True
+                    }
+                    else {
+                        Throw "$_ is either not a valid network adapter of it's currently disabled."
+                    }
+        })]
+        [string]$LocalInterfaceName,
+
+        [Parameter(Mandatory)]
+        [bool]$Enabled
+    )
+
+    BEGIN {
+        if ((Get-NetAdapter | Get-MrInternetConnectionSharing).SharingEnabled -contains $true -and $Enabled) {
+            Write-Warning -Message 'Unable to continue due to Internet connection sharing already being enabled for one or more network adapters.'
+            Break
+        }
+
+        regsvr32.exe /s hnetcfg.dll
+        $netShare = New-Object -ComObject HNetCfg.HNetShare
+    }
+    
+    PROCESS {
+        
+        $publicConnection = $netShare.EnumEveryConnection |
+        Where-Object {
+            $netShare.NetConnectionProps.Invoke($_).Name -eq $InternetInterfaceName
+        }
+
+        $publicConfig = $netShare.INetSharingConfigurationForINetConnection.Invoke($publicConnection)
+
+        if ($PSBoundParameters.LocalInterfaceName) {
+            $privateConnection = $netShare.EnumEveryConnection |
+            Where-Object {
+                $netShare.NetConnectionProps.Invoke($_).Name -eq $LocalInterfaceName
+            }
+
+            $privateConfig = $netShare.INetSharingConfigurationForINetConnection.Invoke($privateConnection)
+        } 
+        
+        if ($Enabled) {
+            $publicConfig.EnableSharing(0)
+            if ($PSBoundParameters.LocalInterfaceName) {
+                $privateConfig.EnableSharing(1)
+            }
+        }
+        else {
+            $publicConfig.DisableSharing()
+            if ($PSBoundParameters.LocalInterfaceName) {
+                $privateConfig.DisableSharing()
+            }
+        }
+
+    }
+
+}
